26346f7b8d6313b3770cb23012fdac3b01cfead2
Valeska <valeskabagaric@hotmail.com>
2022-01-28 19:19:04 -0700

    Editing Variable Script

--- MyFirstProject2022/Assets/Scripts/Assignments/VariablesConditionals/Variables.cs
+++ MyFirstProject2022/Assets/Scripts/Assignments/VariablesConditionals/Variables.cs
@@ -8,7 +8,12 @@ public class Variables : MonoBehaviour
 public int money; //Whole number value 2, 14, 57, -92, -101
 public float change = 0.75f; // Floating point number or decimal 3.15, 105.34, -299.87
 public bool isDay = true; //Booleans are true or false
-private string loginName = "Bob2345"; // String are alpha numeric text contained it quotation marks
+public string loginName = "Bob2345"; // String are alpha numeric text contained it quotation marks
+
+//Homework
+
+public int health;
+public float lives = 10f;
 
 
 
@@ -17,12 +22,28 @@ private string loginName = "Bob2345"; // String are alpha numeric text contained
     {
         money = 125;
         Debug.Log("Money in account = "+ money);
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(loginName == "Bob2345")
+        {
+            print("You have logged in");
+        }
+        else
+        {
+            Debug.Log("Invalid Login name");
+        }
+         if(change <= 1.00f)
+        {
+        print("You do not have enough money!");
+        }
+        else if(change > 1.01f)
+        {
+            Debug.Log("You have made a purchase");
+        }
         if(isDay == true)
         {
             print("It is Day!");
