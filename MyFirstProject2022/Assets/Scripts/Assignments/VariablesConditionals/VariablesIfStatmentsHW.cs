b0529c8fcee99416c1c8c997706faddb9e7123d8
Valeska <valeskabagaric@hotmail.com>
2022-02-05 16:23:38 -0700

    Edit

--- MyFirstProject2022/Assets/Scripts/Assignments/VariablesConditionals/VariablesIfStatmentsHW.cs
+++ MyFirstProject2022/Assets/Scripts/Assignments/VariablesConditionals/VariablesIfStatmentsHW.cs
@@ -22,21 +22,52 @@ public bool youArehappy = true;
         score = 10;
         Debug.Log("Score =" + score);
 
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(health <= 0 )
+        {
+            Debug.Log("Game Over");
+        }
+
+
+        if(score >= 0)
+        {
+            Debug.Log("Level 1");
+        }
+        else if (score > 10)
+        {
+            Debug.Log("Level 2");
+        }
+
+
+        if(speed <= 0.00f)
+        {
+        print("you have stopped");
+        }
+        else if(speed > 0.01f)
+        {
+            Debug.Log("accelerating");
+        }
+
+
         if (favFood == "Sushi")
         {
             Debug.Log("Yum!");
         }
+        else
+        {
+            Debug.Log("Wrong!");
+        }
+
+
         if (youArehappy == true)
         {
             Debug.Log("You are happy!!!");
         }
-        else if(youArehappy == false)
+        else
         {
             Debug.Log("You are sad, feel better soon!");
         }
