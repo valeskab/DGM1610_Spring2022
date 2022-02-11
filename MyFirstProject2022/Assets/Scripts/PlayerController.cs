7beb50284381ed47967d50fb09e2dbb136e408e7
Valeska <valeskabagaric@hotmail.com>
2022-02-10 20:39:43 -0700

    Practice

--- MyFirstProject2022/Assets/Scripts/PlayerController.cs
+++ MyFirstProject2022/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public float turnSpeed = 50.0f;
+    public float hInput;
+    public float fInput;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +18,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Translate(X,Y,Z)
-        transform.Translate(0,0,1);
+        hInput = Input.GetAxis("Horizontal");
+        // Makes the player move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform. Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
