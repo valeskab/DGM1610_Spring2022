cb1a7c3be6d6305083b7dd6c618b2d4fbb2dca99
Valeska <valeskabagaric@hotmail.com>
2022-02-14 03:14:14 -0700

    Basic Math Calculator

--- MyFirstProject2022/Assets/Scripts/Assignments/Operators/SimpleCalculator.cs
+++ MyFirstProject2022/Assets/Scripts/Assignments/Operators/SimpleCalculator.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class SimpleCalculator : MonoBehaviour
 {
+    // Access Modifier DataType Name;
+    public int numberA = 10;
+    public int numberB = 33;
+    public int numberC = 7;
+
+    public float numberD = 33.0f;
+    public float numberE = 7.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Add();
+        Sub();
+        Mult();
+        Div();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Add()
+    {
+        int total;
+
+        total = numberA + numberB;
+
+        Debug.Log(numberA +" + "+ numberB +" = "+ total);
+
+    }
+
+    void Sub()
+    {
+        int total;
+
+        total = numberB - numberC;
+
+        Debug.Log(numberB +" - "+ numberC +" = "+ total);
+    }
+
+    void Mult()
+    {
+        int total;
+
+        total = numberB * numberB;
+
+        Debug.Log(numberB +" x "+ numberB +" = "+ total);
+    }
+
+    void Div()
+    {
+        int total;
+        int rem;
+        float tot;
+
+        total = numberB / numberC;
+        rem = numberB % numberC;
+        tot = numberD / numberE;
+        
+        Debug.Log(numberB +" / "+ numberC +" = "+ total);
+        Debug.Log("Remainder =" + rem);
+        Debug.Log(numberD +" / "+ numberE +" = "+ tot);
     }
 }
