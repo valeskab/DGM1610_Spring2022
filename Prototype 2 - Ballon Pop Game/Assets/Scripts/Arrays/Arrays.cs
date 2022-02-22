64f661c328744cad057e7b6335796eb38a60fe66
Valeska <valeskabagaric@hotmail.com>
2022-02-21 23:09:35 -0700

    Arrays HW

---
+++ Prototype 2 - Ballon Pop Game/Assets/Scripts/Arrays/Arrays.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public string[] friendNames = {"Bob", "Jessica", "Jody", "Arthur", "Randall", "Todd", "Becky"};
+    public int[] numbers = {23,34,56,67,78,97,45,10};
+    //                            { 0, 1, 2, 3, 4, 5, 6}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        Debug.Log("Former friend"+ friendNames[0]);
+        Debug.Log(friendNames[3]+"Says Hi as he walks to class");
+        friendNames[0] = "Veronica";
+        Debug.Log("New friend"+ friendNames[0]);
+        */
+        foreach(int numbers in numbers)
+        {
+            print(numbers);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prototype 2 - Ballon Pop Game/Assets/Scripts/Arrays/ArraysHW.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysHW : MonoBehaviour
+{
+    public string[] makeupBag = {"Mirror", "Lipstick", "Mascara", "Blush", "Foundation", "Concealer", "Brushes", "Powder", "Eyeliner", "Eyeshadow"};
+    //                            { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
