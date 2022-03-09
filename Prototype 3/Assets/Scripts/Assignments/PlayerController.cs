f4f63a82f4ddde71a351d625a7db84ed073601fd
Valeska <valeskabagaric@hotmail.com>
2022-03-08 23:56:39 -0700

    Added Player Controller

--- Prototype 3/Assets/Scripts/Assignments/PlayerController.cs
+++ Prototype 3/Assets/Scripts/Assignments/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float moveSpeed = 5f; // Speed at which the player will move
+    private Rigidbody2D rb; // Store the referenced 2D rigidbody
+    Vector2 movement; //Store the players x,y position movement
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal"); // Input for left right movement
+        movement.y = Input.GetAxis("Vertical"); // Input for Up Down movement
+    }
+
+    // Set number of calls per frame
+
+    FixedUpdate()
+    {
+        // Apply physics and move the character
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
     }
 }
