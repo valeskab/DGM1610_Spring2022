ffd19eb8a1988f7601cce84f5ef9af8fb497de89
Valeska <valeskabagaric@hotmail.com>
2022-03-15 03:15:13 -0600

    Edit follow script

--- Prototype 3/Assets/Scripts/Assignments/Follow.cs
+++ Prototype 3/Assets/Scripts/Assignments/Follow.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
+
+    public float speed;
+    public float stopDistance;
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectsWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
