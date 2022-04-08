7a7d6237af894fdff45a786c07b1928bfcd09731
Valeska <valeskabagaric@hotmail.com>
2022-04-07 20:15:45 -0600

    Switches

--- 2D Rogue Like/Assets/Scripts/Assignments/Switches/Switches.cs
+++ 2D Rogue Like/Assets/Scripts/Assignments/Switches/Switches.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 public class Switches : MonoBehaviour
 {
     public string npc;
-    
+
+    if(npc == "Money for a poor soul")
+    {
+        if(playerSays == "no")
+        {
+
+        }
+        else
+    }
 }
