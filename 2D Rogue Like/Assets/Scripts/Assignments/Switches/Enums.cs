7c9a8b82332d9e03c0b20c36fefa9535ecf1e4b8
Valeska <valeskabagaric@hotmail.com>
2022-04-10 14:23:19 -0600

    Update Code

--- 2D Rogue Like/Assets/Scripts/Assignments/Switches/Enums.cs
+++ 2D Rogue Like/Assets/Scripts/Assignments/Switches/Enums.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Enums : MonoBehaviour
 {
+    enum Color {blue, red, yellow, green, orange, purple};
+    enum CarMake {Oldsmobile, Chevrolet, Ford, Mazda, Toyota, Dodge}
+    enum CarModel {Alero, Cruze, Tacoma, Mustang, Miata, Viper};
+    enum HighFashion {Gucci, Prada, Louisvouitton, Tiffany, Balenciaga, Channel, Dior}
+    enum GameCompanies {Nintendo, Microsoft, Sony, Ubisoft, EA, Activision, SquareEnix}
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       /* Color paint;
+        paint = Color.purple;
+
+        print(paint);
+        */
+
+        CarMake make;
+
+        CarModel model;
+
+        make = CarMake.Dodge;
+        model = CarModel.Viper;
+
+        // print("Car Manufacture:" + make + "Model:" + model);
+
+        Recall(CarMake.Mazda, CarModel.Miata);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void Recall(CarMake make, CarModel model)
+    {
+        if(make == CarMake.Oldsmobile && model == CarModel.Alero)
+        {
+            print("THere is a recall on your vehicle due to a faulty ignition, please take your car to the nearest dealer for repairs");
+        }
+        else if(make == CarMake.Ford && model == CarModel.Mustang)
+        {
+            print("There is a recall on all Mustangs for being too cool to drive!");
+        }
+        else
+        {
+            print("There is no recall notice for your behicle make and model");
+        }
     }
 }
--- 2D Rogue Like/Assets/Scripts/Assignments/Switches/Switches.cs
+++ 2D Rogue Like/Assets/Scripts/Assignments/Switches/Switches.cs
@@ -6,12 +6,46 @@ public class Switches : MonoBehaviour
 {
     public string npcConv;
 
-    Start()
+    public int socialClass;
+
+    void Start()
     {
         switch(npcConv)
         {
             case "Hello":
-            print
+             print("Hello good citizen! May the blessings of the zandar be upon you!");
+             break;
+            case "Goodbye":
+             print("Goodbye! Safe travels to you and to your party!");
+             break;
+            case "We are being attacked":
+             print("You run away in shame, you coward!");
+             break;
+        }
+
+        switch(socialClass)
+        {
+            case 5:
+            Debug.Log("Hello, Your Magesty!");
+            break;
+            case 4:
+            Debug.Log("Hello Young Princess!");
+            break;
+            case 3:
+            Debug.Log("Good Evening my Lord");
+            break;
+            case 2:
+            Debug.Log("Hello, Mayor!");
+            break;
+            case 1:
+            Debug.Log("May I purchase some of your fine chesses?");
+            break;
+            case 0:
+            Debug.Log("Get away from me your riff raff!");
+            break;
+            default:
+            Debug.Log("I do not talk with stangers!!!!");
+            break;
         }
     }
 }
