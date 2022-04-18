bfaf7bd3562282464bcf8b492bf2e8ccdf8f3050
Valeska <valeskabagaric@hotmail.com>
2022-04-17 19:29:47 -0600

    Loops

--- Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
+    //public int numEwoks = 0;
+    public int ducksInPond = 99;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+        for(int i = 99; i > numEwoks; i--)
+        {
+            Debug.Log("There are " + i + " Ewoks on a log!");
+        }
+        */
+
+        while(ducksInPond > 0)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south of the winter");
+            ducksInPond --;
+        }
     }
 
     // Update is called once per frame
