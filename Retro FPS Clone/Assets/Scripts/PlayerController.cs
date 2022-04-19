c46dbb51909d83111efeed5ae8923fb905474633
Valeska <valeskabagaric@hotmail.com>
2022-04-18 20:38:20 -0600

    Edit

--- Retro FPS Clone/Assets/Scripts/PlayerController.cs
+++ Retro FPS Clone/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,16 @@ public class PlayerController : MonoBehaviour
     private Vector2 mouseInput;
     public float mouseSensitivity = 1.0f;
 
+    public Camera viewCam;
+
+    public GameObject projectileImpact;
+    public int currentAmmo;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +32,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // Getting keyboard input
         Vector3 moveH = transform.right * moveInput.x;
         Vector3 moveV = transform.forward * moveInput.z;
         rb.velocity = (moveH + moveV) * moveSpeed;
 
         mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z );
+        
+        if(currentAmmo> 0)
+        {
+            Ray ray = 
+        }
     }
 }
