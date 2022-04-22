c8c96e55082c12347bf1d141ca96f2cf6d3f3191
Valeska <valeskabagaric@hotmail.com>
2022-04-21 22:21:54 -0600

    for

--- Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
-    //public int numEwoks = 0;
+    /*
+    public int numEwoks = 0;
+    public int numCookie = 25;
+    public int numCoins = 0;
+    public int numLives = 5;
+    public int numFriends = 0;
+
+    */
+    
     public int ducksInPond = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         /*
-        for(int i = 99; i > numEwoks; i--)
+
+        ////For////
+
+        for(int i = 99; i >= numEwoks; i--)
         {
             Debug.Log("There are " + i + " Ewoks on a log!");
         }
-        */
+
+        for(int i = 1; i <= numCookie; i++) // ++ increments plus one, counting up
+        {
+            Debug.Log("There are " + i + " Cookies in the jar!");
+        }
+
+        for(int i = 100; i >= numCoins; i--) // -- counting down
+        {
+            Debug.Log("You purchased this item. Now you have " + i + "coins!");
+        }
+
+        for(int i = 1; i <= numLives; i++)
+        {
+            Debug.Log("You ate the magic mushroom! now you have " + i + " lives!");
+        }
+
+        for(int i = 47; i >= numFriends; i--)
+        {
+            Debug.Log("You now have" + i + " Friends");
+        }
+        
+        ////While////
 
         while(ducksInPond < 99)
         {
@@ -23,6 +55,36 @@ public class Loops : MonoBehaviour
             Debug.Log("All of the ducks have flown south of the winter");
             ducksInPond ++;
         }
+
+        while(ducksInPond < 99)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south of the winter");
+            ducksInPond ++;
+        }
+
+        while(ducksInPond < 99)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south of the winter");
+            ducksInPond ++;
+        }
+
+        while(ducksInPond < 99)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south of the winter");
+            ducksInPond ++;
+        }
+
+        while(ducksInPond < 99)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south of the winter");
+            ducksInPond ++;
+        }
+        */
+
     }
 
     // Update is called once per frame
