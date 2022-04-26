f1e90dde21a8e861566d75e8329b1f35a3c66054
Valeska <valeskabagaric@hotmail.com>
2022-04-26 16:48:32 -0600

    Added Player Controller

---
+++ Final Personal Game Project - Oven Fresh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("PlayerStats")]
+    public float speed;
+    public float jumpHeight;
+    private Rigidbody2D rb;
+
+    [Header("GroundCheck")]
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private float moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        rb = GetComponent<Rigidbody2D>(); //Loading the rigidbody
+    }
+
+    void Update()
+    {
+        //Ground check sensor, This is going to make the radius go in a circle to check if the player is touching the ground.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); 
+        
+        // Non-stick Player
+        moveVelocity = 0f;
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+
+        // Move player left and right
+        rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
+
+        //Get key down only registers it once, get key would only go down permanently
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+}
