64aa7ea2fa4ddcca0b8cb699bd48ec09f2999ff2
Valeska <valeskabagaric@hotmail.com>
2022-04-28 20:19:41 -0600

    EnemyPatrol

---
+++ Final Personal Game Project - Oven Fresh/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+     public float speed;
+    public float distance;
+    private bool moveRight = true;
+
+    public Transform groundDetection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the enemy to the right
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if(groundInfo.collider == false)
+        {
+            if(moveRight == true)
+            {
+                // Flip Enemy at edge to move left
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveRight = false;
+            }
+            else
+            {
+                // Flip Enemy at edge to move right
+                transform.eulerAngles = new Vector3(0,0,0);
+                moveRight = true;
+            }
+        }
+
+    }
+}
